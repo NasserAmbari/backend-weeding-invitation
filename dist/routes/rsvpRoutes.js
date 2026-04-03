f0b275caf686ee92cbdaffbd75747f5cae178ccf
Ahmad Nasser Ambari <nssr.mbr@gmail.com>
2026-04-04 03:27:20 +0800

    Delete dist

--- dist/routes/rsvpRoutes.js
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import { RsvpController } from "../controllers/rsvpController.js";
-import { authMiddleware } from "../middleware/authMiddleware.js";
-const router = Router();
-router.post("/", RsvpController.create);
-router.get("/", authMiddleware, RsvpController.getAll);
-export default router;
--- dist/server.js
+++
@@ -1,5 +0,0 @@
-import app from "./app.js";
-const port = process.env.PORT ? parseInt(process.env.PORT) : 3000;
-app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
-});
